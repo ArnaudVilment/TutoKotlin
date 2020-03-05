8fb14303567cac982c7d067887c3c749d1d61104
arnaud <arnaud.vilment@open-groupe.com>
2020-03-05 16:27:35 +0100

    Firt commit

---
+++ src/model/Course.kt
@@ -0,0 +1,6 @@
+package com.example.dao
+
+data class Course(var id: Int,
+                  var title: String,
+                  var level: Int,
+                  var isActif: Boolean)
---
+++ src/model/Error.kt
@@ -0,0 +1,3 @@
+package com.example.model
+
+data class Error(val status: Int, val message: String)
